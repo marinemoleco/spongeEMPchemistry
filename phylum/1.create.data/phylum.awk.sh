1e29a3aad8ffbcd299a7c75aed93df9a14ff863c
marinemoleco <34373107+marinemoleco@users.noreply.github.com>
2018-08-02 10:36:42 +0200

    Delete phylum.awk.sh

--- phylum/1.create.data/phylum.awk.sh
+++
@@ -1,3 +0,0 @@
-sed -i '1d' phylum_samples.txt
-awk -F'\t' 'NR==FNR{arr[$1]++;next}{for(i=1; i<=NF; i++) if ($i in arr){a[i]++;}} { for (i in a) printf "%s\t", $i; printf "\n"}' phylum_samples.txt <(zcat final.withtax.v14.tsv.tar.gz) > final.phylum.txt
-
