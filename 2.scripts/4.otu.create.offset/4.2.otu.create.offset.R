ede673f099585117868c3f93290b7a206cf83e66
marinemoleco <34373107+marinemoleco@users.noreply.github.com>
2018-08-21 11:22:51 +0200

    Add files via upload

--- 2.scripts/4.otu.create.offset/4.2.otu.create.offset.R
+++ 2.scripts/4.otu.create.offset/4.2.otu.create.offset.R
@@ -2,14 +2,14 @@
 
 ##paths
 
-dir <- "3.results/class.create.data.output/"
+dir <- "3.results/otu.create.data.output/"
 
 #after running 1.filter.awk.sh
 
-read.table(file="2.scripts/4.class.create.offset/final.class.txt", sep="\t", header=TRUE, check.names = FALSE) -> emp.orig_all # this is the data for the offset-calculation-file-table
-load(file="3.results/class.create.data.output/factors.RData")
+read.table(file="2.scripts/4.otu.create.offset/final.otu.txt", sep="\t", header=TRUE, check.names = FALSE) -> emp.orig_all # this is the data for the offset-calculation-file-table
+load(file="3.results/otu.create.data.output/factors.RData")
 
-emp.orig <- droplevels(emp.orig_all[,-2446]) # the awk script adds an NA column to the end, hence I need to remove that here
+emp.orig <- droplevels(emp.orig_all[,-2420]) # the awk script adds an NA column to the end, hence I need to remove that here
 
 emp.sum <- apply(emp.orig, 2, sum, na.rm=TRUE)
 emp.sum.df <- as.data.frame(emp.sum)
@@ -18,5 +18,5 @@ rownames(emp.sum.df) <- emp.sum.df[,1]
 emp.sum.df[,1] <- NULL
 emp.sum.final <- emp.sum.df[,1]
 
-save(emp.sum.final, file = paste(dir, "emp.sum.final.class", ".RData", sep = ""))
+save(emp.sum.final, file = paste(dir, "emp.sum.final.otu", ".RData", sep = ""))
 rm(dir)
