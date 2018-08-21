cf4c34f7ae1b30986e50faf2e92e191a78c34f06
marinemoleco <34373107+marinemoleco@users.noreply.github.com>
2018-08-21 10:56:45 +0200

    Add files via upload

---
+++ 2.scripts/4.class.create.offset/4.1.class.awk.sh
@@ -0,0 +1,1 @@
+awk -F'\t' 'NR==FNR{arr[$1]++;next}{for(i=1; i<=NF; i++) if ($i in arr){a[i]++;}} { for (i in a) printf "%s\t", $i; printf "\n"}' class_samples.txt <(zcat final.withtax.v14.tsv.tar.gz) > final.class.txt
